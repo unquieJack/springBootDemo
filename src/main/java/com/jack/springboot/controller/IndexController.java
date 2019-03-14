6c34ce41025a74f7f00c04f48457a394ec33dfa0
Jack4765 <69934765@qq.com>
2019-03-14 14:16:42 +0800

    test commint

--- src/main/java/com/jack/springboot/controller/IndexController.java
+++ src/main/java/com/jack/springboot/controller/IndexController.java
@@ -16,13 +16,13 @@ public class IndexController {
     @RequestMapping("/index")
     public String index() throws Exception{
         System.out.println("1111111111113333123123");
-        simpleMailSend.simpleSend("444316747@qq.com","黄慧你好","你爱我吗？");
+        simpleMailSend.simpleSend("69934765@qq.com","test","hello mail");
 
-        simpleMailSend.sendWithHtml("444316747@qq.com","黄慧你好","你爱我吗？");
+        simpleMailSend.sendWithHtml("69934765@qq.com","test","hello mail");
 
-        templateMailSend.sendWithThymeleafHtmlTemplate("444316747@qq.com","test1","mail/html/mail-hello","html","logo.png");
+        templateMailSend.sendWithThymeleafHtmlTemplate("69934765@qq.com","test1","mail/html/mail-hello","html","logo.png");
 
-        templateMailSend.sendWithThymeleafTextTemplate("444316747@qq.com","test2","mail/text/mail-hello","txt");
+        templateMailSend.sendWithThymeleafTextTemplate("69934765@qq.com","test2","mail/text/mail-hello","txt");
         return "index" ;
     }
 }
